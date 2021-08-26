8918c4590fda62d625d864139b587e8c7a39adf2
Azhar-WomenInDigital <azhar.womenindigital@gmail.com>
2021-08-26 16:14:38 +0600

    gallery system upload

---
+++ app/Models/Gallerypic.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallerypic extends Model
+{
+    protected $fillable = ['gallery_pic_name', 'gallery_pic'];
+}
---
+++ database/migrations/2021_08_26_092318_create_gallerypics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGallerypicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gallerypics', function (Blueprint $table) {
+            $table->id();
+            $table->string('gallery_pic_name');
+            $table->string('gallery_pic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallerypics');
+    }
+}
